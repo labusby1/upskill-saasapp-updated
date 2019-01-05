341f38d266d18e7334db2f479c26d46877be32f8
Luke Busby <labusby1@gmail.com>
2019-01-05 17:06:21 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+  # Form validations for contact form
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
