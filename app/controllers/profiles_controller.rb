eb4fe45d3af082e11cfc2984887eb4161a58ce9f
Luke Busby <labusby1@gmail.com>
2019-01-11 14:07:02 +0000

    Added User Profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # render a blank profiles details form
+    @profile = Profile.new
   end
 end
